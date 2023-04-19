434da6f722bf43f034846393b2acf17af10e8db3
caudaplant <caudaplant@gmail.com>
2023-04-19 10:10:32 +0900

    [add]ページを増やす

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+// import '@/styles/globals.css';
+import Layout from '@/components/layout';
+// import type { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+// export default function App({ Component, pageProps }: AppProps) {
+//   return <Component {...pageProps} />
+// }
+
+function MyApp({ Component, pageProps }) {
+    return (
+        <Layout>
+            <Component {...pageProps} />
+        </Layout>
+    );
 }
+
+export default MyApp;
