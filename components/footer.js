65da7252543f18013fe8636b59aa0373c31cfda2
caudaplant <caudaplant@gmail.com>
2023-04-21 22:27:42 +0900

    [edit]メタ設定

--- components/footer.js
+++ components/footer.js
@@ -1,14 +1,18 @@
+import Container from '@/components/container'
 import Logo from '@/components/logo'
+import Social from '@/components/social'
 import styles from '@/styles/footer.module.scss'
 
 export default function Footer()
 {
     return (
         <footer className={styles.wrapper}>
-            <div className={styles.flexContainer}>
-                <Logo />
-                [ソーシャル]
-            </div>
+            <Container>
+                <div className={styles.flexContainer}>
+                    <Logo />
+                    <Social />
+                </div>
+            </Container>
         </footer>
     )
 }
---
+++ components/post-body.js
@@ -0,0 +1,10 @@
+import styles from '@/styles/post-body.module.scss';
+
+export default function PostBody({children})
+{
+    return (
+        <div className={styles.stack}>
+            {children}
+        </div>
+    )
+}
